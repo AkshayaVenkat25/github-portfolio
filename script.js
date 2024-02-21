43e5b95f00c5c2caead311f15021f2454dc145f5
AkshayaVenkat25 <akshaya25venkat@gmail.com>
2024-02-21 23:20:16 +0530

    icons

--- script.js
+++ script.js
@@ -1,76 +1,62 @@
+// SIDEBAR FUNCTIONALITIES
+
+// TOGGLE
 function toggleSidebar() {
     var sidebar = document.getElementById("mySidebar");
     sidebar.style.display = (sidebar.style.display === "none") ? "block" : "none";
 }
 
+// CLOSE
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
 
-// Add these functions to your existing JavaScript code
-
-// Add these functions to your existing JavaScript code
-
+// RESUME NAVIGATION
 function navigateToResume() {
-    // Get the resume link
-    var resumeLink = document.getElementById('resumeLink').href;
-    
-    // Open the resume link in a new tab/window
+   var resumeLink = document.getElementById('resumeLink').href;
     window.open(resumeLink, '_blank');
 }
 
+// SKILLS
 function navigateToSkills() {
-    // Get the skills section offset top value
-    var skillsSectionOffsetTop = document.getElementById('skills').offsetTop;
-
-    // Scroll to the skills section with smooth behavior
-    window.scrollTo({
+      var skillsSectionOffsetTop = document.getElementById('skills').offsetTop;
+      window.scrollTo({
         top: skillsSectionOffsetTop,
         behavior: 'smooth'
     });
 
-    // Close the sidebar
     w3_close();
 }
 
+// EXPERIENCE
 function navigateToExperience() {
-    // Get the experience section offset top value
     var experienceSectionOffsetTop = document.getElementById('experience').offsetTop;
-
-    // Scroll to the experience section with smooth behavior
     window.scrollTo({
         top: experienceSectionOffsetTop,
         behavior: 'smooth'
     });
 
-    // Close the sidebar
     w3_close();
 }
 
+// PROJECTS
 function navigateToProjects() {
-    // Get the projects section offset top value
     var projectsSectionOffsetTop = document.getElementById('projects').offsetTop;
-
-    // Scroll to the projects section with smooth behavior
     window.scrollTo({
         top: projectsSectionOffsetTop,
         behavior: 'smooth'
     });
 
-    // Close the sidebar
     w3_close();
 }
 
+// CONTACT
 function navigateToContact() {
-    // Get the contact section offset top value
     var contactSectionOffsetTop = document.getElementById('contact').offsetTop;
-
-    // Scroll to the contact section with smooth behavior
     window.scrollTo({
         top: contactSectionOffsetTop,
         behavior: 'smooth'
     });
 
-    // Close the sidebar
     w3_close();
 }
